5b35a805abf6d581b569a26e7e262409a79095d6
Earl Zachary <hack@metahack.com>
2015-01-23 12:01:05 -0800

    added title

---
+++ scratch3.py
@@ -0,0 +1,14 @@
+import sys
+from tkinter import *
+#import tkinter
+
+print("hi")
+
+mGui = Tk()
+
+mGui.geometry('450x450+200+200')
+
+mGui.title("Global Thermonuclear War Simulator")
+
+mGui.mainloop()
+
