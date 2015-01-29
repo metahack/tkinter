cb5442bf4a5323eec71e50f9600e6f3863abba45
Earl Zachary <hack@metahack.com>
2015-01-29 14:03:16 -0800

    about to delete mGuiGrid1 as redundant

--- scratch3.py
+++ scratch3.py
@@ -4,39 +4,55 @@ from tkinter import *
 
 
 def mHello():
-    mLabel3 = Label(mGui2, text="hi").pack()
-    mGui2.lift()
-    mGui.focus_force()
+    mLabel3["text"] = ment.get()
+ 
+    # optional code to bring window to top and change focus
+    mGuiButton.lift()
+    mGuiButton.focus_force()
 
-print("hi")
-
-mGui = Tk()
-mGui2 = Tk()
-mGuiGrid = Tk()
+mGuiMain = Tk()
+mGuiButton = Tk()
+mGuiGrid1 = Tk()
 mGuiGrid2 = Tk()
 
-mGui.geometry('450x450+200+200')
-mGui2.geometry('200x100+100+100')
-mGuiGrid.geometry('200x100+700+100')
+mGuiMain.geometry('450x450+200+200')
+mGuiButton.geometry('200x100+100+100')
+mGuiGrid1.geometry('200x100+700+100')
 
 # note: no defaullt size means grid dimensions determine window size
 mGuiGrid2.geometry('+1000+100')
 
-mGui.title("Global Thermonuclear War Simulator")
+mGuiMain.title("Main")
+mGuiGrid1.title("Grid 1")
+mGuiGrid2.title("Grid 2")
+mGuiButton.title("Button")
+
+ment = StringVar(value="hi")
 
 
 
-mLabel = Label(text="my text", fg="red", bg="black").pack()
+
+mLabel = Label(text="my text", fg="red", bg="black")
+mLabel.pack()
 mLabel2 = Label(text="text again", fg="gold", bg="white")
 mLabel2.place(x=225,y=100)
-mButton = Button(mGui2, text="click here", command = lambda: Label(mGui2, text="greetings").pack()).pack()
+mLabel3 = Label(mGuiButton, text=ment.get())
+mLabel3.pack()
+
+
+mButton = Button(mGuiButton, text="click here", command = lambda: Label(mGuiButton, text="greetings").pack())
+mButton.pack()
+
+mButton2 = Button(mGuiMain, text="click here", command = mHello)
+mButton2.pack()
+
 
 # note: empty rows and columns take up zero space
-mLabelGrid1 = Label(mGuiGrid, text="0-3", bg='pink').grid(row=0, column=3)
-mLabelGrid2 = Label(mGuiGrid, text="7-2", bg='white').grid(row=7, column=2)
-mLabelGrid3 = Label(mGuiGrid, text="1-1", bg='gold').grid(row=1, column=1)
-mLabelGrid4 = Label(mGuiGrid, text="0-0").grid(row=0, column=0)
-mLabelGrid5 = Label(mGuiGrid, text="3-5").grid(row=3, column=5)
+mLabelGrid1 = Label(mGuiGrid1, text="0-3", bg='pink').grid(row=0, column=3)
+mLabelGrid2 = Label(mGuiGrid1, text="7-2", bg='white').grid(row=7, column=2)
+mLabelGrid3 = Label(mGuiGrid1, text="1-1", bg='gold').grid(row=1, column=1)
+mLabelGrid4 = Label(mGuiGrid1, text="0-0").grid(row=0, column=0)
+mLabelGrid5 = Label(mGuiGrid1, text="3-5").grid(row=3, column=5)
 
 mLabelGrid2_1 = Label(mGuiGrid2, text="one-three", bg='pink').grid(row=0, column=3)
 mLabelGrid2_2 = Label(mGuiGrid2, text="seven-two", bg='white').grid(row=7, column=2)
@@ -48,5 +64,11 @@ mLabelGrid5_5 = Label(mGuiGrid2, text="three-five").grid(row=3, column=5)
 mLabelGrid5_6 = Label(mGuiGrid2, text="3-2", bg='olive').grid(row=3, column=2, sticky=W)
 mLabelGrid5_7 = Label(mGuiGrid2, text="2-1", bg='light slate grey').grid(row=2, column=1, sticky=E)
 
-mGui.mainloop()
+# textbox entry
+entry = Entry(mGuiMain, textvariable=ment)
+entry.pack()
+
+
+
+mGuiMain.mainloop()
 
