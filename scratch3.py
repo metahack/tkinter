1a5efb8672b27bb32b3a2be9d47959fa547346ae
Earl Zachary <hack@metahack.com>
2015-01-31 10:43:48 -0800

    about to refactor so each window's code is in its own place'

--- scratch3.py
+++ scratch3.py
@@ -1,5 +1,6 @@
 import sys
 from tkinter import *
+
 #import tkinter
 
 
@@ -10,21 +11,48 @@ def mHello():
     mGuiButton.lift()
     mGuiButton.focus_force()
 
+def mInfo():
+    messagebox.showinfo(title="info", message="Mules have no reproductive organs.")
+    return
+
+
 mGuiMain = Tk()
+
+# menubars for GuiMain
+menubar = Menu(mGuiMain)
+
+filemenu = Menu(menubar, tearoff = 0)
+fileMenu2 = Menu(menubar)
+subMenu2 = Menu(fileMenu2)
+
+subMenu2.add_command(label="restart stub")
+subMenu2.add_command(label="reformat drive stub")
+fileMenu2.add_cascade(label="extreme options", menu = subMenu2)
+
+filemenu.add_command(label="info", command = mInfo)
+filemenu.add_command(label="search", command = lambda: messagebox.showinfo(title="search stub", message="hi"))
+menubar.add_cascade(label="file", menu=filemenu)
+
+fileMenu2.add_command(label="quit", command = quit)
+menubar.add_cascade(label="main", menu=fileMenu2)
+
+mGuiMain.config(menu=menubar)
+
+
+
 mGuiButton = Tk()
-mGuiGrid1 = Tk()
+
 mGuiGrid2 = Tk()
 
 mGuiMain.geometry('450x450+200+200')
 mGuiButton.geometry('200x100+100+100')
-mGuiGrid1.geometry('200x100+700+100')
 
-# note: no defaullt size means grid dimensions determine window size
+# note: no default size means grid dimensions determine window size
 mGuiGrid2.geometry('+1000+100')
 
 mGuiMain.title("Main")
-mGuiGrid1.title("Grid 1")
-mGuiGrid2.title("Grid 2")
+
+mGuiGrid2.title("Grid")
 mGuiButton.title("Button")
 
 ment = StringVar(value="hi")
@@ -48,13 +76,7 @@ mButton2.pack()
 
 
 # note: empty rows and columns take up zero space
-mLabelGrid1 = Label(mGuiGrid1, text="0-3", bg='pink').grid(row=0, column=3)
-mLabelGrid2 = Label(mGuiGrid1, text="7-2", bg='white').grid(row=7, column=2)
-mLabelGrid3 = Label(mGuiGrid1, text="1-1", bg='gold').grid(row=1, column=1)
-mLabelGrid4 = Label(mGuiGrid1, text="0-0").grid(row=0, column=0)
-mLabelGrid5 = Label(mGuiGrid1, text="3-5").grid(row=3, column=5)
-
-mLabelGrid2_1 = Label(mGuiGrid2, text="one-three", bg='pink').grid(row=0, column=3)
+mLabelGrid2_1 = Label(mGuiGrid2, text="one-three", bg='pink', font=("courier", 16)).grid(row=0, column=3)
 mLabelGrid2_2 = Label(mGuiGrid2, text="seven-two", bg='white').grid(row=7, column=2)
 mLabelGrid3_3 = Label(mGuiGrid2, text="one-one", bg='gold').grid(row=1, column=1)
 mLabelGrid4_4 = Label(mGuiGrid2, text="zero-zero").grid(row=0, column=0)
@@ -68,7 +90,9 @@ mLabelGrid5_7 = Label(mGuiGrid2, text="2-1", bg='light slate grey').grid(row=2, 
 entry = Entry(mGuiMain, textvariable=ment)
 entry.pack()
 
-
+Button(mGuiMain, text="quit", command = quit).place(x=400, y=20)
 
 mGuiMain.mainloop()
+
+       
 
